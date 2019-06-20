30462084cb24aaf9470f868c97d26f82eeb69c42
biryuk <biryuk@exchange.informatik.uni-hamburg.de>
2019-06-20 15:44:55 +0200

    ⚗️TRY NEW BUILD SETUP FOR TEST COVERAGE GENERATION

--- run_docker.py
+++ run_docker.py
@@ -4,9 +4,15 @@ author:     Volodymyr Biryuk
 
 The module to run the micro service in a production environment (used by Docker).
 """
+import os
+
 import microservice
 
 
 if __name__ == '__main__':
     app = microservice.create_app()
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    filename = os.path.join(dirname, 'coverage-reports')
+    for file in os.listdir(filename):
+        app.logger.info(file)
     app.run(host=app.config['MS_HOST'], port=int(app.config['MS_PORT']))
