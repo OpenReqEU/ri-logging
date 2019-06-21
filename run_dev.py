fd6d6331b8d18b9bbd63eaa5a44a414cf7f4077c
biryuk <biryuk@exchange.informatik.uni-hamburg.de>
2019-06-21 15:45:02 +0200

    ♻️ SIMPLIFY THE WAY HOW THE CONFIG IS LOADED IN A DEV ENVIRONMENT

--- run_dev.py
+++ run_dev.py
@@ -9,30 +9,5 @@ import os
 import microservice
 
 if __name__ == '__main__':
-    # Uncomment and fill information into the following lines if the config file needs to be recreated on every startup.
-    try:
-        os.remove('config.json')
-    except FileNotFoundError:
-        pass
-    os.environ['MS_HOST'] = ''
-    os.environ['DB_HOST'] = ''
-    os.environ['DB_PORT'] = ''
-    os.environ['DB_AUTH_MECHANISM'] = ''
-    os.environ['DB_AUTH_SOURCE'] = ''
-    os.environ['DB_NAME_FRONTEND_LOGS'] = ''
-    os.environ['DB_USER'] = ''
-    os.environ['DB_PASSWORD'] = ''
-    os.environ['DB_CONNECTION_TIMEOUT'] = ''
-
-    os.environ['API_URL'] = ''
-    os.environ['USER_BEARER_TOKEN'] = ''
-    os.environ['ADMIN_BEARER_TOKEN'] = ''
-    os.environ['DIR_DEBUG_LOG'] = ''
-    os.environ['DIR_BACKEND_LOG'] = ''
-    os.environ['BACKEND_LOG_SCHEDULE'] = ''
-
-    os.environ['DEBUG'] = ''
-    os.environ['LOGGING_LEVEL'] = ''
-
-    app = microservice.create_app()
+    app = microservice.create_app('config_dev.json')
     app.run(host=app.config['MS_HOST'], port=int(app.config['MS_PORT']))
