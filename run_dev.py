82cf009a1cdcf96207b69ac1b2f6b7d9c3866ef4
Volodymyr Biryuk <biryuk@exchange.informatik.uni-hamburg.de>
2019-07-31 12:13:45 +0200

    ADD: EXCEPTION HANDLING IN RUN FILE

--- run_dev.py
+++ run_dev.py
@@ -9,5 +9,12 @@ import os
 import microservice
 
 if __name__ == '__main__':
-    app = microservice.create_app('config_dev.json')
+    app = microservice.create_app()
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    try:
+        filename = os.path.join(dirname, 'coverage-reports')
+        for file in os.listdir(filename):
+            app.logger.info(file)
+    except FileNotFoundError as e:
+        pass
     app.run(host=app.config['MS_HOST'], port=int(app.config['MS_PORT']))
